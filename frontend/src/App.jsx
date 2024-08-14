8fcbd35ab9ccae74ac89f57d5220146071b1a309
nikitachoudhary <nikitachoudhary364@gmail.com>
2024-08-14 16:53:45 +0530

    footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className='text-3xl bg-green-600'>hello</div>
+      
+      <Footer/>
     </>
   )
 }
